f5cd5a4341e3e504ec959c3c1539ea4cae1841d0
eduardonk9999 <thiago.novak7@gmail.com>
2021-09-07 16:34:24 -0300

    Add, aula2

---
+++ Estrutura-do-programa/app.js
@@ -0,0 +1,16 @@
+var caughy = 5 * 5
+
+// Função
+/**
+ * Função é um pedaço de programa envolvido por um valor
+ * Ex: 
+ */
+alert('Sou uma funcao')
+
+
+// Execução condicional
+var theNumber = Number(prompt("Pick a number", ""))
+if(!isNaN(theNumber)) {
+    alert("Your number is the square root of " + theNumber + theNumber)
+}
+
